451ad24cb68933b5d23bcd78ce3ad936434c1a16
zhangmin <815422863@qq.com>
2021-12-10 09:59:44 +0800

    update config

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
  * @Author: zhangmin
  * @Date: 2021-05-19 08:38:33
  * @LastEditors: zhangmin
- * @LastEditTime: 2021-11-26 17:30:56
+ * @LastEditTime: 2021-12-10 09:59:06
  * @Description: 接口地址配置
  */
 
@@ -12,9 +12,6 @@ switch (process.env.NODE_ENV) {
   case "development":
     baseUrl = "https://expert-library.ruiztech.cn/ruiz"; //本地的请求url
     break;
-    case "qym":
-      baseUrl = "https://expert-library.ruiztech.cn/ruiz"; //本地的请求url
-      break;
   case "test": // 注意这里的名字要和设置的环境名字对应起来
     baseUrl = "https://expert-library.ruiztech.cn/ruiz"; //测试环境中的url
     break;
